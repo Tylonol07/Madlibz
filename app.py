9d5348ec3c2d806f49433cd142a8a23de0a7a42c
Tylon <tjhunt0707@gmail.com>
2025-07-08 12:03:28 -0700

    finished Gorcery Store madlib

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("My first repo")
+#Tylons MadLibZ
+
+adj1 = input("give me an adjective ")
+Pnoun = input("give me a Plural Noun ")
+Noun1 = input("give me a Noun ")
+rela1 = input("gve me a Relative ")
+adj2 = input("give me an Adjective ")
+Pnoun2 = input("give me a Plural noun ")
+adj3 = input("give me an Adjective ")
+Noun2 =input("gve me a Noun")
+adj4 = input("give me an Adjective")
+Noun3 = input("give me a Noun")
+rela2 = input("give e a  Relative")
+
+
+print("One", adj1,  "morning, the", Pnoun ,"went to the",Noun1 ,"to do their weekly shopping. As they walked down the aisles, they bumped into their", rela1, "who was wearing a hilarious",adj2, "costume . Suddenly, a group of", Pnoun2, "started singing karaoke in the produce section, much to the surprise of the shoppers. Trying to navigate the chaos, they accidentally knocked over a display of", adj3, Noun2, "causing a domino effect throughout the store. The manager, a", adj4 ,Noun3, "came rushing over to assess the situation and ended up getting caught in a silly string fight with the mischievous", rela2,". In the end, everyone couldn't help but laugh at the ridiculousness of their unexpected trip to the supermarket.")
